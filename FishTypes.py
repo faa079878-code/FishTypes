90d2afcb3a33058edd9eff79534e54bc18f54ee0
faa079878-code <faa079878@gmail.com>
2025-10-30 11:31:19 +0400

    Update FishTypes.py

--- FishTypes.py
+++ FishTypes.py
@@ -32,9 +32,10 @@ set_background("Background.jpg")
 # App title
 st.title("Ecotype Distribution")
 st.markdown(
-    '<h1 style="text-align: right;">مخطط توزيع الأنماط البيئية للأسماك</h1>',
+    '<h1 style="text-align: right; margin-right: 20px;">مخطط توزيع الأنماط البيئية للأسماك</h1>',
     unsafe_allow_html=True
 )
+
 
 
 # Arabic Text (Right-Aligned)
@@ -111,6 +112,7 @@ labels_rtl = [get_display(arabic_reshaper.reshape(cat)) for cat in categories]
 ax.legend(bbox_to_anchor=(1.05, 1), loc='upper left', labels=labels_rtl)
 
 st.pyplot(fig)
+
 
 
 
