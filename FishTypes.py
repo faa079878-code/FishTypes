e71690265a4f7f9605ef0e889dc3426c06f4c229
faa079878-code <faa079878@gmail.com>
2025-10-30 10:06:05 +0400

    Update FishTypes.py

--- FishTypes.py
+++ FishTypes.py
@@ -48,6 +48,10 @@ for group in groups:
             group_data[cat] = val
             total += val
 
+ if total != 100:
+        st.warning(f"⚠️ مجموع النسب لمجموعة **{group}** هو {total}%. يُفضل أن يكون المجموع = 100%.")
+    data[group] = group_data
+
 # --- Convert to DataFrame ---
 df = pd.DataFrame(data)
 
